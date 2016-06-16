5d12df1e938e79f93a0891b0dd4c01a992fcfa14
Gabe Boning <gboning@gmail.com>
2016-06-16 09:03:45 +0200

    CORS allow

--- app.js
+++ app.js
@@ -37,6 +37,13 @@ function validateTokenMiddleware (req, res, next) {
 // enforce token on every request
 app.use(validateTokenMiddleware);
 
+// allow cross-origin requests
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function(req,res) {
   res.send("");
 });
